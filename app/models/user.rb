308a65480711ead206ca81513e5a9e6a8a5c6979
Mark Anthony Gibbins <xiy3x0@gmail.com>
2025-03-10 05:43:29 +0000

    fix: only calculate stats for joined channels

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,8 @@ class User < ApplicationRecord
   has_many :channels, through: :channel_users
 
   def messages_since_last_sent
-    Message.where("created_at > ?", last_sent_at)
+    Message.where(channel_id: channels.pluck(:id))
+      .where("created_at > ?", last_sent_at)
       .where.not(user_id: id)
       .where("created_at > ?", messages.maximum(:created_at) || last_sent_at)
   end
